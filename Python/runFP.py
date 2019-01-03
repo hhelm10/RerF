5c9f6a3e70b606d0f9839cd9021cb17ad2517d0d
Ben Falk <falk.ben@gmail.com>
2019-01-03 17:39:16 -0500

    additional scaffolding

---
+++ Python/runFP.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+# load the iris.csv data
+iris = np.genfromtxt('data/iris.csv', delimiter=',')
+
+# import the python library for rerf
+# import rerf
+
+# run rerf on the iris dataset using base random forest
+# zs = fastRerF("data/iris.csv", 4, forestType="rfBase")
+
+# run rerf on the iris dataset using randomer forest
+# zs = fastRerF("data/iris.csv", 4, forestType="rerf")
+
+# print the parameters
+# zs.printParameters()
+# zs.printForestType()
+
+# run fastPredict
+# predictions = fastPredict(iris[:,1:4], zs)
+
+# print predictions
+# print(predictions)
