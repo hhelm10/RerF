7f6f1675c6112534c693a13a4f74a2ef24a11cfc
Ben Falk <falk.ben@gmail.com>
2019-01-04 17:20:28 -0500

    current progress -- errors in building due to templating and pointers

---
+++ Python/packedForest/fp.pyx
@@ -0,0 +1,34 @@
+# distutils: language = c++
+# cython: language_level=3
+
+from fpForest cimport fpForest
+
+cdef class fp:
+    cdef fpForest c_fpForest
+
+    def __cinit__(self):
+        self.c_fpForest = fpForest()
+
+    def setParamString(self, parameterName, parameterValue):
+        self.c_fpForest.setParamString(parameterName, parameterValue)
+
+    def setParamDouble(self, parameterName, parameterValue):
+        self.c_fpForest.setParamDouble(parameterName, parameterValue)
+
+    def setParamInt(self, parameterName, parameterValue):
+        self.c_fpForest.setParamInt(parameterName, parameterValue)
+
+    def printParameters(self):
+        self.c_fpForest.printParameters()
+
+    def printForestType(self):
+        self.c_fpForest.printForestType()
+
+    def growForest(self):
+        self.c_fpForest.growForest()
+
+    def predict(self, observation):
+        return self.c_fpForest.predict(observation)
+
+    def testAccuracy(self):
+        return self.c_fpForest.testAccuracy()
